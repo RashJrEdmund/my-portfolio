b35e4061de9740795f387af744cf4e8ab792dd5b
rash <orashusedmund@gmail.com>
2024-05-30 06:09:42 +0100

    finished footer

--- src/core/components/molecules/footer/footer.tsx
+++ src/core/components/molecules/footer/footer.tsx
@@ -8,7 +8,7 @@ function HandleCard({ handle: { id, name, username, href } }: { handle: SocialHa
   return (
     <li className="flex flex-col">
       <p className="font-semibold text-[1rem]">{name}</p>
-      <p className="font-light">
+      <p className="font-light text-nowrap">
         @{username}
 
         <Link href={href} target="_blank" className="ml-2 sm:ml-4 w-fit" title={"go to " + name + " - " + username}>
@@ -23,12 +23,12 @@ export default async function Footer() {
   const SocialHandles = await getSocials();
 
   return (
-    <footer id="contact" className="w-full h-fit min-h-[20vh] self-end flex flex-col items-center justify-center gap-8 bg-app-dark-600">
-      <div className="w-full max-w-app-dynamic-max-w mx-auto flex items-start justify-between gap-8 py-8">
-        <HeaderLogo hTagLevel="h2" />
+    <footer id="contact" className="w-full h-fit min-h-[20vh] mt-12 self-end flex flex-col items-center justify-center gap-8 bg-app-dark-600">
+      <div className="w-full max-w-app-dynamic-max-w mx-auto flex flex-col md:flex-row items-start justify-between gap-8 py-8">
+        <HeaderLogo hTagLevel="h2" imgSx="inline" />
 
-        <div className="w-fit sm:w-full flex items-center justify-center">
-          <div>
+        <div className="w-fit sm:w-full flex items-start justify-center gap-[5px] sm:gap-8 md:gap-16">
+          <div className="w-fit">
             <h3 className="font-bold sm:text-[1.2rem] mb-4">
               <SmallLineSvg className="mr-4" />
               Link tree
@@ -48,6 +48,43 @@ export default async function Footer() {
               }
             </ul>
           </div>
+
+          <div className="w-fit">
+            <h3 className="font-bold sm:text-[1.2rem] mb-4 text-nowrap">
+              <SmallLineSvg className="mr-4" />
+              Others
+            </h3>
+
+            <ul className="flex flex-col gap-y-2 sm:gap-y-4">
+              {
+                [
+                  {
+                    name: "Email",
+                    val: "orashusedmund@gmail.com",
+                  },
+                  {
+                    name: "Phone Number",
+                    val: "+237 670 118 208",
+                  }
+                ].map(({ name, val }) => (
+                  <li className="flex flex-col" key={name}>
+                    <p className="font-semibold text-[1rem] text-nowrap">{name}</p>
+                    <p className="font-light break-all">
+                      {val}
+                    </p>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-app-dynamic-max-w">
+        <hr className="border-app-blue-500" />
+
+        <div className="py-6">
+          Made with 💙 by <span>Rash</span>
         </div>
       </div>
     </footer>
--- src/core/components/ui/text-logo.tsx
+++ src/core/components/ui/text-logo.tsx
@@ -1,13 +1,19 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-function HeaderLogo({ hTagLevel: Htag = "h1" }: { hTagLevel?: keyof JSX.IntrinsicElements }) {
+interface Props {
+  hTagLevel?: keyof JSX.IntrinsicElements;
+  imgSx?: string;
+}
+
+function HeaderLogo({ hTagLevel: Htag = "h1", imgSx = "" }: Props) {
   return (
     <Htag className="font-semibold sm:text-[1.5rem] w-fit">
       <Link
         title="Rash for short"
         href="/"
-        className="w-fit hover:text-app-blue-500 duration-300"
+        className="w-fit hover:text-app-blue-500 duration-300 text-nowrap"
       >
         Orashus <span className="text-app-blue-500 text-[2rem] sm:text-[4rem] leading-[0]">.</span>
 
@@ -16,7 +22,7 @@ function HeaderLogo({ hTagLevel: Htag = "h1" }: { hTagLevel?: keyof JSX.Intrinsi
           alt="rash edmund"
           width={40}
           height={40}
-          className="rounded-full hidden sm:inline"
+          className={cn("rounded-full hidden sm:inline", imgSx)}
         />
       </Link>
     </Htag>
