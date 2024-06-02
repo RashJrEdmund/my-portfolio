d1ddf860037c4c5a14816fe52f41d408524e2e9a
rash <orashusedmund@gmail.com>
2024-06-02 13:17:12 +0100

    added full stops "." to SkillData

--- src/core/features/pages/api/dummy-data.ts
+++ src/core/features/pages/api/dummy-data.ts
@@ -5,25 +5,25 @@ const SkillData: Skill[] = [
     id: "1",
     label: "Attention to details",
     description:
-      "A have a keep eye not just for design, but for code smells, and properly coding patterns",
+      "A have a keep eye not just for design, but for code smells, and properly coding patterns.",
   },
   {
     id: "2",
     label: "Teachable",
     description:
-      "I am teachable and will adhere to good council, both in and out of the programming world",
+      "I am teachable and will adhere to good council, both in and out of the programming world.",
   },
   {
     id: "3",
     label: "Logical Approach",
     description:
-      "I always strive to tackle a problem as the best way I can. Constantly seeking for ways to improve code quality and efficiency",
+      "I always strive to tackle a problem as the best way I can. Constantly seeking for ways to improve code quality and efficiency.",
   },
   {
     id: "4",
     label: "Creative Ability",
     description:
-      "A key factor all developers, front and back, should posses. I do believe that everything and anything can be made intuitive and fun to work with",
+      "A key factor all developers, front and back, should posses. I do believe that everything and anything can be made intuitive and fun to work with.",
   },
 ];
 
